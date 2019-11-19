3bbe2cc30cef079ab1678e9fad64b48ba4709ea9
Unknown <theo.mariotte@gmail.com>
2019-11-19 14:42:22 +0100

    Modif test_jn + add getoutFile

--- _tests/test_jn_hn.m
+++ _tests/test_jn_hn.m
@@ -30,28 +30,30 @@ dr = .01;
 r_in = 0 : dr : 20;
 % r_in = 2;
 % max function order
-n = 6;
+n = 0;
 
 % Spherical Bessel function
 [hn,dhn,r] = SphericalHankel1(n,r_in);
-[jn,djn,~,~,~] = SphericalBessel(n,r_in);
+[jn,djn,yn,dyn,~] = SphericalBessel(n,r_in);
 
 % test validity of the differentiation using Wronksian relation 
 % (i.e. jn(r)hn'(r) - jn'(r)hn(r) = i/r^2, i complex)
 Gn = jn .* dhn - hn .* djn;
+Gn_2 = jn .* dyn - yn .* djn;
 
 % theoretical value
-Gn_th = 1i./(r.^2);
+Gn_th = 1i./(r(1:2:end).^2);
+Gn_th2 = 1./(r(1:2:end).^2);
 
 % squared error
-e_r = abs(real(Gn_th - Gn)).^2;
-e_i = abs(imag(Gn_th - Gn)).^2;
+% e_r = abs(Gn_th2 - Gn_2).^2;
+% e_i = abs(imag(Gn_th - Gn)).^2;
 
-RMSE_r = sqrt(mean(e_r));
-RMSE_i = sqrt(mean(e_i));
+% RMSE_r = sqrt(mean(e_r));
+% RMSE_i = sqrt(mean(e_i));
 
-fprintf('RMSE real part : %.4g\n',RMSE_r)
-fprintf('RMSE imaginary part : %.4g\n',RMSE_i)
+% fprintf('RMSE real part : %.4g\n',RMSE_r)
+% fprintf('RMSE imaginary part : %.4g\n',RMSE_i)
 
 %% Hankel function (real and imaginary parts) + derivatives
 
@@ -64,26 +66,52 @@ RadialFuncVisu(hn,r,pp);
 
 %% Comparison Gn and Gn_th
 
-h = figure('Name','Comparison');
+h = figure('Name','Bessel 1 et 2');
 
-subplot(2,1,1)
 hold on
-plot(r,real(Gn),'k','linewidth',2)
-plot(r,real(Gn_th),'k','linewidth',2,'linestyle','--')
-plot(r,e_r,'r','linewidth',2)
-legend('Estimated','Theoretical','squared error')
-grid on
+plot(r(1:2:end),Gn_th2,'k','linewidth',2,'markersize',7);
+plot(r,Gn_2,'rx','linewidth',1);
 hold off
 
-subplot(2,1,2)
-hold on
-plot(r,imag(Gn),'k','linewidth',2)
-plot(r,imag(Gn_th),'k','linewidth',2,'linestyle','--')
-plot(r,e_i,'r','linewidth',2)
-set(gca,'ylim',[-200 200])
-legend('Estimated','Theoretical','squared error')
+set(gca,'yscale','log','ylim',[1e-1 1e3],'xlim',[0 3])
+set(gca,'fontsize',12);
 grid on
+
+title('Wronskien : $j_n(r)$ et $y_n(r)$','interpreter','latex')
+xlabel('Rayon $r$','interpreter','latex');
+ylabel("$G_{n}(r) = j_n(r)y_n'(r) - j_n'(r)y_n(r)$",'interpreter','latex')
+legend('Theorie','Calcul')
+
+impath = 'H:\Mes documents\5A\Projet_5A\Images\';
+fname = 'Wronskien_jn_yn';
+
+outName = getOutFileName(impath,fname,'.eps');
+print(h,outName,'-depsc');
+
+%%%
+
+h4 = figure('Name','Bessel Hankel');
+
+hold on
+plot(r(1:2:end),imag(Gn_th),'k','linewidth',2,'markersize',7)
+plot(r,imag(Gn),'rx','linewidth',1)
 hold off
+
+set(gca,'yscale','log')
+set(gca,'fontsize',12);
+set(gca,'ylim',[1e-1 1000],'xlim',[0 3])
+grid on
+
+title('Wronskien : $j_n(r)$ et $h_n(r)$','interpreter','latex')
+xlabel('Rayon $r$','interpreter','latex');
+ylabel("$G_{n}(r) = j_n(r)h_n'(r) - j_n'(r)h_n(r)$",'interpreter','latex')
+legend('Theorie','Calcul')
+
+fname = 'Wronskien_jn_hn';
+
+outName = getOutFileName(impath,fname,'.eps');
+print(h4,outName,'-depsc');
+
 
 
 
---
+++ tools/getOutFileName.m
@@ -0,0 +1,22 @@
+function out_file_name = getOutFileName(Path,fname,ext)
+
+if ~exist(Path,'dir')
+    warning('Path does not exist. Creating new directory.');
+    mkdir(Path);
+end
+
+if ext(1) ~= '.'
+   ext = ['.' ext]; 
+end
+
+fID = 1;
+fname = [fname sprintf('_%02d',fID)];
+
+while(exist([Path fname ext],'file'))
+    fID = fID + 1;
+    fname = [fname sprintf('_%02d',fID)];
+end
+
+out_file_name =[Path fname ext];
+
+end
